8253002546478f843183eb6126422c83cb00e98f
Daniel <danielcp62.dc@gmail.com>
2025-10-15 12:19:06 -0500

    Update Repository/ Empleado y Restaurante

--- backend/src/main/java/com/appetize/repository/EmpleadoRepository.java
+++ backend/src/main/java/com/appetize/repository/EmpleadoRepository.java
@@ -2,6 +2,11 @@ package com.appetize.repository;
 
 import com.appetize.model.entity.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface EmpleadoRepository extends JpaRepository<Empleado, String> {
+    Optional<Empleado> findByNombre(String name);
 }
--- backend/src/main/java/com/appetize/repository/RestauranteRepository.java
+++ backend/src/main/java/com/appetize/repository/RestauranteRepository.java
@@ -2,6 +2,8 @@ package com.appetize.repository;
 
 import com.appetize.model.entity.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, String> {
 }
