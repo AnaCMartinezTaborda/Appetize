14b0ff78d4435d69e51acd2240dd4d99f293bc88
Daniel <danielcp62.dc@gmail.com>
2025-10-15 12:03:56 -0500

    Creación de entidades de Empleado y Restaurante

---
+++ backend/src/main/java/com/appetize/model/entity/Restaurante.java
@@ -0,0 +1,29 @@
+package com.appetize.model.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "restaurantes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true)
+    @NotBlank(message = "El nombre no puede estar vacío")
+    private String nombre;
+
+    @NotBlank(message = "El teléfono no puede estar vacío")
+    @Pattern(regexp = "^3\\d{9}$", message = "Ingrese un numero de teléfono correcto")
+    private String telefono;
+}
