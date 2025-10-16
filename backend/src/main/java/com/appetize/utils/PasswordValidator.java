49a6884567db9a6c9af37108ff03c6392c2df241
Daniel <danielcp62.dc@gmail.com>
2025-10-15 21:39:47 -0500

    Se modifica el PasswordValidator

--- backend/src/main/java/com/appetize/utils/PasswordValidator.java
+++ backend/src/main/java/com/appetize/utils/PasswordValidator.java
@@ -3,13 +3,6 @@ package com.appetize.utils;
 import java.util.regex.Pattern;
 
 public class PasswordValidator {
-
-    // Regex moderno para contraseña segura:
-    // - mínimo 8 caracteres
-    // - al menos una mayúscula
-    // - al menos una minúscula
-    // - al menos un número
-    // - al menos un carácter especial
     private static final String PASSWORD_REGEX =
             "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}:;\"',.<>?]).{8,}$";
 
