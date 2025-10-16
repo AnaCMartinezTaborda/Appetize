4de064015f8fc6f8c055d87165666a5427481938
Daniel <danielcp62.dc@gmail.com>
2025-10-16 12:18:25 -0500

    Configuración para Deploy

--- backend/src/main/java/com/appetize/controller/AuthController.java
+++ backend/src/main/java/com/appetize/controller/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
     private final EmpleadoService service;
 
     @PostMapping("/register")
