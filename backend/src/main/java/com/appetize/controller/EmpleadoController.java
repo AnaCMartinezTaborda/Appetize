30b93514ed39df621813f8432a623714a776256f
Daniel <danielcp62.dc@gmail.com>
2025-10-19 14:16:43 -0500

    Se ajusta summary de swagger a controladores

--- backend/src/main/java/com/appetize/controller/EmpleadoController.java
+++ backend/src/main/java/com/appetize/controller/EmpleadoController.java
@@ -2,6 +2,7 @@ package com.appetize.controller;
 
 import com.appetize.model.dto.request.empleado.EmpleadoRequest;
 import com.appetize.service.abstraction.EmpleadoService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ public class EmpleadoController {
 
     private final EmpleadoService service;
 
+    @Operation(summary = "Crear un nuevo empleado")
     @PostMapping
     public ResponseEntity<String> createEmpleado(@RequestBody EmpleadoRequest request){
         service.createEmpleado(request);
