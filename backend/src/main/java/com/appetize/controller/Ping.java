ca28ec3d4534a45a94745095f44e48a4d05d26a4
Daniel <danielcp62.dc@gmail.com>
2025-10-28 13:21:09 -0500

    Update Ping

--- backend/src/main/java/com/appetize/controller/Ping.java
+++ backend/src/main/java/com/appetize/controller/Ping.java
@@ -5,15 +5,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/ping")
 public class Ping {
 
+    private static final Logger log = LoggerFactory.getLogger(Ping.class);
+
     @Operation(summary = "endpoint para mantener vivo el deploy")
     @GetMapping
     public ResponseEntity<?> ping(){
-        System.out.println("Ping Recibido");
+        log.info("Ping recibido");
         return ResponseEntity.ok().build();
     }
 }
