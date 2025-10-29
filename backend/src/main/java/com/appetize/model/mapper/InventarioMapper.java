3cdab3b155c235fd906a27360cb9adeadf68e9d9
Daniel <danielcp62.dc@gmail.com>
2025-10-29 15:11:25 -0500

    Update InventarioMapper: Se importa el mapper de restaurante

--- backend/src/main/java/com/appetize/model/mapper/InventarioMapper.java
+++ backend/src/main/java/com/appetize/model/mapper/InventarioMapper.java
@@ -1,34 +1,26 @@
 package com.appetize.model.mapper;
 
 import com.appetize.model.dto.response.InventarioResponse;
-import com.appetize.model.dto.response.RestauranteResponse;
 import com.appetize.model.entity.Inventario;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class InventarioMapper {
 
+    private final RestauranteMapper restauranteMapper;
+
     public InventarioResponse entityToDto(Inventario entity){
-
-        RestauranteResponse restaurante = RestauranteResponse.builder()
-                .id(entity.getRestaurante().getId())
-                .nombre(entity.getRestaurante().getNombre())
-                .email(entity.getRestaurante().getEmail())
-                .direccion(entity.getRestaurante().getDireccion())
-                .telefono(entity.getRestaurante().getTelefono())
-                .createdAt(entity.getRestaurante().getCreatedAt())
-                .updatedAt(entity.getRestaurante().getUpdatedAt())
-                .build();
-
         return InventarioResponse.builder()
                 .id(entity.getId())
                 .idExterno(entity.getIdExterno())
                 .nombre(entity.getNombre())
                 .cantidad(entity.getCantidad())
-                .precioUnitario(entity.getPrecioUnitario())
+                .precioUnitario(entity.getCostoUnitario())
                 .tipoInventario(entity.getTipoInventario())
                 .unidadMedida(entity.getUnidadMedida())
-                .restaurante(restaurante)
+                .restaurante(restauranteMapper.entityToDto(entity.getRestaurante()))
                 .build();
     }
 }
