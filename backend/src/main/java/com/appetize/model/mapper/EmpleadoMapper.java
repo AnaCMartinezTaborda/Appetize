ea59e1ae1442e46aa6e8f14ec580bedf902ce7df
Daniel <danielcp62.dc@gmail.com>
2025-10-29 15:11:02 -0500

    Update EmpleadoMapper: Se importa el mapper de restaurante

--- backend/src/main/java/com/appetize/model/mapper/EmpleadoMapper.java
+++ backend/src/main/java/com/appetize/model/mapper/EmpleadoMapper.java
@@ -1,31 +1,23 @@
 package com.appetize.model.mapper;
 
 import com.appetize.model.dto.response.EmpleadoResponse;
-import com.appetize.model.dto.response.RestauranteResponse;
 import com.appetize.model.entity.Empleado;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class EmpleadoMapper {
 
+    private final RestauranteMapper restauranteMapper;
+
     public EmpleadoResponse entityToDto(Empleado entity){
-
-        RestauranteResponse restaurante = RestauranteResponse.builder()
-                .id(entity.getRestaurante().getId())
-                .nombre(entity.getRestaurante().getNombre())
-                .email(entity.getRestaurante().getEmail())
-                .direccion(entity.getRestaurante().getDireccion())
-                .telefono(entity.getRestaurante().getTelefono())
-                .createdAt(entity.getRestaurante().getCreatedAt())
-                .updatedAt(entity.getRestaurante().getUpdatedAt())
-                .build();
-
         return EmpleadoResponse.builder()
                 .id(entity.getId())
                 .tipo(entity.getTipo())
                 .cedula(entity.getCedula())
                 .nombre(entity.getNombre())
-                .restaurante(restaurante)
+                .restaurante(restauranteMapper.entityToDto(entity.getRestaurante()))
                 .createdAt(entity.getCreatedAt())
                 .updatedAt(entity.getUpdatedAt())
                 .lastSession(entity.getLastSession())
