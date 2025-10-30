0be80338b759728e00c4c225ca235acf25529e79
Daniel <danielcp62.dc@gmail.com>
2025-10-30 15:31:28 -0500

    Se agregan anotaciones de validación a los Requests

--- backend/src/main/java/com/appetize/model/dto/request/auth/RegisterRequest.java
+++ backend/src/main/java/com/appetize/model/dto/request/auth/RegisterRequest.java
@@ -1,5 +1,8 @@
 package com.appetize.model.dto.request.auth;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +13,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class RegisterRequest {
+    @NotBlank(message = "El nombre no puede estar vacío")
     String nombre;
+
+    @NotBlank(message = "La cédula no puede estar vacía")
     String cedula;
+
+    @NotBlank(message = "La contraseña no puede estar vacía")
+    @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).+$",
+            message = "La contraseña debe tener una mayúscula, una minúscula, un número y un carácter especial"
+    )
     String password;
 }
--- backend/src/main/java/com/appetize/model/dto/request/restaurante/RestauranteRequest.java
+++ backend/src/main/java/com/appetize/model/dto/request/restaurante/RestauranteRequest.java
@@ -1,5 +1,8 @@
 package com.appetize.model.dto.request.restaurante;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +13,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class RestauranteRequest {
+
+    @NotBlank(message = "El nombre no puede estar vacío")
     private String nombre;
+
+    @NotBlank(message = "El correo electrónico no puede estar vacío")
+    @Email(message = "El correo electrónico no es válido")
     private String email;
+
+
     private String direccion;
+
+    @NotBlank(message = "El teléfono no puede estar vacío")
+    @Pattern(
+            regexp = "^(\\+?57)?\\d{10}$",
+            message = "El número de teléfono no es válido"
+    )
     private String telefono;
 }
