deec66dbc0b2def2c02ef4eaf21932b37fc548f5
DanielContreras21 <danielcp62.dc@gmail.com>
2026-02-02 14:22:36 -0500

    Update: se eliminan restricciones de @Valid en RegisterRequest para tener control las constraints

--- backend/src/main/java/com/appetize/model/dto/request/auth/RegisterRequest.java
+++ backend/src/main/java/com/appetize/model/dto/request/auth/RegisterRequest.java
@@ -1,8 +1,5 @@
 package com.appetize.model.dto.request.auth;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,17 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class RegisterRequest {
-    @NotBlank(message = "El nombre no puede estar vacío")
     String nombre;
-
-    @NotBlank(message = "La cédula no puede estar vacía")
     String cedula;
-
-    @NotBlank(message = "La contraseña no puede estar vacía")
-    @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
-    @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).+$",
-            message = "La contraseña debe tener una mayúscula, una minúscula, un número y un carácter especial"
-    )
     String password;
 }
